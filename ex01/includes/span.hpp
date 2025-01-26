4bd850bef6a14d6e169586e6e3c03f10c509ac38
Mo0x <max.govindaraju@gmail.com>
2025-01-26 19:15:17 +0100

    ex01 start

---
+++ ex01/includes/span.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   span.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgovinda <mgovinda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/26 18:53:51 by mgovinda          #+#    #+#             */
+/*   Updated: 2025/01/26 19:15:04 by mgovinda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+#include <exception>
+#include <vector>
+
+class span
+{
+	private:
+	std::vector<unsigned int>	v;
+	unsigned int				size;
+
+	public:
+
+};
+
+#endif
